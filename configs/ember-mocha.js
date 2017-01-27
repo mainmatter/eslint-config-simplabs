927b8acf91c6194bf25cf778358f2932c4ec5ee6
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-27 16:16:05 +0100

    config/ember-mocha: Disable "prefer-arrow-callback" rule

--- configs/ember-mocha.js
+++ configs/ember-mocha.js
@@ -7,6 +7,10 @@ module.exports = {
     'simplabs/configs/ember',
   ],
   rules: {
+    // disabled to allow e.g. `expect(foo).to.be.true;`
     'no-unused-expressions': 'off',
+
+    // disabled to allow `function() {...}` in `it()` blocks
+    'prefer-arrow-callback': 'off',
   },
 };
