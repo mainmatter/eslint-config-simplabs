196766f8bac2d99af3ad51796b33287c2baed571
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-01 12:47:51 +0100

    Add blueprint acceptance test

---
+++ node-tests/blueprints/eslint-config-simplabs-test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+var setupTestHooks = blueprintHelpers.setupTestHooks;
+var emberNew = blueprintHelpers.emberNew;
+var emberGenerate = blueprintHelpers.emberGenerate;
+
+let { expect, file } = require('ember-cli-blueprint-test-helpers/chai');
+
+describe('eslint-config-simplabs blueprint', function() {
+  setupTestHooks(this);
+
+  it('generates .eslintrc.js files', function() {
+    return emberNew()
+      .then(() => emberGenerate(['eslint-config-simplabs']))
+      .then(() => {
+        expect(file('.eslintrc.js')).to.contain('extends: \'simplabs\'');
+        expect(file('tests/.eslintrc.js')).to.contain('extends: \'simplabs/configs/ember-qunit\'');
+      });
+  });
+});
